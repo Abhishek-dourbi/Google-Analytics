47011105955fc0f73d6b502b6285dd5dd8be5893
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-11-09 00:11:39 +0530

    add buttons to the UI

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,52 +8,20 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -69,21 +38,47 @@ const App: () => Node = () => {
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            padding: 20,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              backgroundColor: 'red',
+              marginBottom: 10,
+            }}
+            onPress={() => {}}>
+            <Text>Button One</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              backgroundColor: 'red',
+              marginBottom: 10,
+            }}
+            onPress={() => {}}>
+            <Text>Button Two</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              backgroundColor: 'red',
+              marginBottom: 10,
+            }}
+            onPress={() => {}}>
+            <Text>Button Three</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              backgroundColor: 'red',
+              marginBottom: 10,
+            }}
+            onPress={() => {}}>
+            <Text>Button Four</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
